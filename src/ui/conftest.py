fe1f1880c7eb05fcd5d114d2ac8b5d85a9eb3834
ohuno <ohuno@DEFAULT>
2026-01-18 17:32:25 +0500

    update logged_page with using new architecture (actions, pages) and remove not used fixtures

--- src/ui/conftest.py
+++ src/ui/conftest.py
@@ -1,24 +1,15 @@
 import os
 import pytest
-from playwright.sync_api import Browser, Page
-from ui.components.navigation import Navigation
+from playwright.sync_api import Browser
+from ui.actions.login_actions import LoginActions
 
 
 @pytest.fixture
-def perform_login(settings):
-    def _perform_login(page: Page):
+def logged_page(browser: Browser, settings):
+    base_url = settings.front_end_url
+    storage_path = settings.storage_state_path
 
-        page.get_by_role("textbox", name="Логин").fill(settings["login"])
-        page.get_by_role("textbox", name="Пароль").fill(settings["password"])
-        page.get_by_role("button", name="Войти").click()
-
-    return _perform_login
-
-@pytest.fixture #(scope="session")
-def logged_page(browser: Browser, settings, perform_login):
-    base_url = settings['front_end_url']
-    # To keep session active
-    storage_path = settings["state_path"]
+    # создаём storage файл, если не существует
     if not os.path.exists(storage_path):
         os.makedirs(os.path.dirname(storage_path), exist_ok=True)
         with open(storage_path, "w") as f:
@@ -27,27 +18,21 @@ def logged_page(browser: Browser, settings, perform_login):
     context = browser.new_context(storage_state=storage_path)
     page = context.new_page()
 
-    page.goto(base_url, timeout=60000)
+    page.goto(base_url, timeout=60_000)
 
-
-    # if already logged-in, return the page
-    if page.url == f"{base_url}/users":
+    # если уже залогинены
+    if page.url.endswith("/users"):
         return page
 
     # page.set_viewport_size({"width": 1920, "height": 1080})
     # page.set_default_timeout(60_000)
     # page.set_default_navigation_timeout(60_000)
 
-    perform_login(page)
+    # иначе логиним через Action
+    login_actions = LoginActions(page)
+    login_actions.login_as_user(settings.test_login, settings.test_password)
 
-    # save storage state
+    # сохраняем storage state
     context.storage_state(path=storage_path)
 
     return page
-
-@pytest.fixture
-def navigation(logged_page: Page):
-    nav = Navigation(logged_page)
-    yield nav
-    #post condition -> Close browser
-    logged_page.close()
