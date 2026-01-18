b9437b3198712b3680b467362f33e9ea5735837d
ohuno <ohuno@DEFAULT>
2026-01-18 17:26:48 +0500

    write example action to navigate with user actions

---
+++ src/ui/actions/navigation_actions.py
@@ -0,0 +1,13 @@
+class NavigationActions:
+    """Навигация пользователя по приложению через UI"""
+
+    def __init__(self, page):
+        self.page = page
+
+    def go_to_users_page(self):
+        """Открывает страницу пользователей через меню"""
+        pass
+
+    def go_to_login_page(self):
+        """Переходит на страницу логина через кнопку входа"""
+        pass
